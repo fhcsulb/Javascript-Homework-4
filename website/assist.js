b738d44fd34c2e1c01aed7f43bce20c9619f82cb
Felix <Felix@Felixs-MacBook-Pro.local>
2017-10-11 00:09:32 -0700

    added comments. pyriamid green border won't show now if you enter an invalid number/

--- website/assist.js
+++ website/assist.js
@@ -1,3 +1,4 @@
+//this is a function to prompt the user to enter their numerical input
 function enterNumber(){
     let u_input = Number(prompt("Enter a number between 2 - 10 "));
     console.log(u_input);
@@ -5,11 +6,13 @@ function enterNumber(){
     
 }
 
+//validate that the number was some valid input. 
+//if it wasn't then prompt the user to reload the page. 
 function validateNumber(input){
     if(input <=1 || input>=11)
     {
        
-        document.getElementById("validity").innerHTML = `Your input ${input} was not valid. The valid input number is between 2 and 10. Please reload this page and try again`;
+        document.getElementById("validity").innerHTML = `Your input ${input} was not valid. The valid input number is between 2 and 10. Please reload this page and try again.`;
         document.getElementById("validity").style.color = "red";
        return false;
     }
@@ -22,6 +25,8 @@ function validateNumber(input){
     }
 }
 
+//function that calculates how many times you need to divide by
+//2 to get a value that is smaller than 1 millionth.
 function DivideMillionCount(input){
     let N = input;
     let  count = 0;
@@ -34,12 +39,14 @@ function DivideMillionCount(input){
     document.getElementById("million").innerHTML = `The number of times to divide the number ${input} by 2 to get a value less than one-millionth is ${count}`;
 }
 
+//function that displays the * pyriamid. 
 function displayPyriamid(input){
     let N = input;
     let arr = [];
     let i;
     let k;
     let line;
+    let pyriamid_section
 
     for(i = 0; i < N; i++)
     {
@@ -55,12 +62,16 @@ function displayPyriamid(input){
 
    for (let i in arr)
    {
-      let pyriamid_section = document.getElementById("pyriamid");
+      pyriamid_section = document.getElementById("pyriamid");
       line = document.createElement("p");
       let stars = document.createTextNode(arr[i]);
       line.appendChild(stars);
       pyriamid_section.appendChild(line);
    }
-    
+
+   //manipulate the pyriamid section color and style here. Do this so that 
+   //only if the pyriamid is created will the green border show up.
+    pyriamid_section.style.borderColor = "green";
+    pyriamid_section.style.borderStyle = "solid";
 
 }
